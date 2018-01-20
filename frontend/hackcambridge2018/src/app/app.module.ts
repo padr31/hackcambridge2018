2130ba23250a08f6d395827e09414452903688d7
padr31 <padr31@users.noreply.github.com>
2018-01-20 15:55:50 +0000

    First card

--- frontend/hackcambridge2018/src/app/app.module.ts
+++ frontend/hackcambridge2018/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { CardComponent} from './components/card/card.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
