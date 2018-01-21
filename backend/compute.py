4be5a3603cbae262a72946ecec5ad5f4bd448867
Juraj Mičko <micko.juraj64@gmail.com>
2018-01-21 00:07:12 +0000

    Improve compute algorithm

--- backend/compute.py
+++ backend/compute.py
@@ -6,7 +6,13 @@ def compute(o):
     vals = [evalPerson(p) for p in o]
     if len(vals) == 0: return 0
 
-    avg = sum(vals) / len(vals)
+    weights_sum = sum(weight for (interest, weight) in vals)
+    vals = [(interest, weight / weights_sum) for (interest, weight) in vals]
+
+    for i in range(len(o)):
+        o[i]["weight_normalized"] = vals[i][1]
+
+    avg = sum(interest * weight for (interest, weight) in vals)
     return avg
 
 def evalPerson(p):
@@ -14,15 +20,24 @@ def evalPerson(p):
     r = p['faceRectangle']
     #print(s)
 
-    interest = s['happiness'] ** 2 \
-           - s['sadness'] ** 2 \
-           - s['neutral'] ** 2 \
-           + s['surprise'] ** 2 \
-           + s['fear'] ** 2 \
-           + s['contempt'] ** 2 \
-           - s['disgust'] ** 2 \
-           - s['anger'] ** 2
-    area = r['width'] * r['height']
-    weight = 1 - math.exp(-area)
+    exP = 1/1.2
+    exN = 1/exP
 
-    return interest * weight
+    interest = s['happiness'] ** exP \
+           + s['surprise'] ** exP \
+           + s['fear'] ** exP \
+           + s['contempt'] ** exP \
+           - s['sadness'] ** exN \
+           - s['disgust'] ** exN \
+           - s['anger'] ** exN \
+           - math.exp((s['neutral'] - 1)*4)
+    area = r['width'] * r['height'] / float(4608 * 3456)
+    relative_width = math.sqrt(r['width'] * r['height'] / float(4608 * 3456))
+    #weight = (1 - math.exp(-area * 234)) * (1 - s["neutral"] / 2)
+    weight = relative_width * (1 - s["neutral"] / 2)
+
+    p["weight"] = weight
+    p["interest"] = interest
+    p["relative_width"] = relative_width
+
+    return (interest, weight)
